5354113b7014797fcdb0f1c746530df93ef9cc3d
gaganasahithi <41728944+gaganasahithi@users.noreply.github.com>
2018-07-26 12:05:00 +0100

    Create Ideone1.java

---
+++ sahi/Ideone1.java
@@ -0,0 +1,21 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone1
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+    Scanner sc=new Scanner(System.in);
+    int a=sc.nextInt();
+    if(a%2==0)
+    {
+    System.out.println("a is even");
+    }
+    else if(a%3==0)
+    {
+    System.out.println("a is odd");
+}
+}
+}
