c55b40a93b22e3437c3d33e68f1f43b3249d5e73
gaganasahithi <41728944+gaganasahithi@users.noreply.github.com>
2018-07-27 06:34:15 +0100

    Update sample5.java

--- prgs/sample5.java
+++ prgs/sample5.java
@@ -8,8 +8,7 @@ class sample5
 	public static void main (String[] args) throws java.lang.Exception
 	{
 		Scanner sc=new Scanner(System.in);
-		System.out.println("enter a character");
-		 char ch=sc.next().charAt(0);
+	        char ch=sc.next().charAt(0);
 		 if(ch=='a'|| ch=='e'||ch=='i'||ch=='o'||ch=='u'||ch=='A'||ch=='E'||ch=='I'||ch=='O'||ch=='U')
 		 {
 		 	System.out.println("vowel");
