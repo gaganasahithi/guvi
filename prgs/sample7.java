ec0564ee3e49b30a7b9408a9d92219dd18d7d2b7
gaganasahithi <41728944+gaganasahithi@users.noreply.github.com>
2018-07-27 06:51:24 +0100

    Create sample7.java

---
+++ prgs/sample7.java
@@ -0,0 +1,22 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class sample7
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		char d;
+		Scanner sc=new Scanner(System.in);
+		d=sc.next().charAt(0);
+		if((d>='a' && d<='z') || (d>='A' && d<='Z'))
+		{
+			System.out.println("Alphabet");
+		}
+		else
+		{
+			System.out.println("No");
+		}
+		}
+	}
