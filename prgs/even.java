2d95e85e0d2956062b88ce1c0207d6a9bbc3d06d
gaganasahithi <41728944+gaganasahithi@users.noreply.github.com>
2018-07-31 07:38:42 +0100

    Create even.java

---
+++ prgs/even.java
@@ -0,0 +1,21 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+
+class even
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		int q=sc.nextInt();
+	for(int i=n+1;i<q;i++)
+	{
+		if(i % 2 == 0)
+		{
+			System.out.println(i+" ");
+		}
+	}
+	}
+}
