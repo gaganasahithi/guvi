1f31b0cb87f2f480c0554e811ae1a353199962c0
gaganasahithi <41728944+gaganasahithi@users.noreply.github.com>
2018-07-31 11:34:09 +0100

    Update array.java

--- prgs/array.java
+++ prgs/array.java
@@ -8,13 +8,13 @@ class array
 	public static void main (String[] args) throws java.lang.Exception
 	{
 		int n=5;
-		int k=2;
+		int k=3;
 		int sum=0;
        for(int i=1;i<=5;i++)
        {
 		for(int j=1;j<=k;j++)
 		{
-			sum=j+1;
+			sum=n+1;
 		}	
 		}	
          System.out.println(sum);
