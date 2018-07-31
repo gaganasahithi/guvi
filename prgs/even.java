7594301782906e8051a690bc82ab2c761e8adf84
gaganasahithi <41728944+gaganasahithi@users.noreply.github.com>
2018-07-31 07:59:01 +0100

    Update even.java

--- prgs/even.java
+++ prgs/even.java
@@ -3,19 +3,31 @@ import java.lang.*;
 import java.io.*;
 
 
-class even
+class Ideone
 {
 	public static void main (String[] args) throws java.lang.Exception
 	{
-		Scanner sc=new Scanner(System.in);
-		int n=sc.nextInt();
-		int q=sc.nextInt();
-	for(int i=n+1;i<q;i++)
-	{
-		if(i % 2 == 0)
-		{
-			System.out.println(i+" ");
-		}
-	}
+	int i =0;
+       int num =0;
+       String  primeNumbers = "";
+       for (i = 1; i <= 10; i++)         
+       { 		  	  
+          int counter=0; 	  
+          for(num =i; num>=1; num--)
+	  {
+             if(i%num==0)
+	     {
+ 		counter = counter + 1;
+	     }
+	  }
+	  if (counter ==2)
+	  {
+	     primeNumbers = primeNumbers + i + " ";
+	  }	
+       }	
+       System.out.println(primeNumbers);
 	}
 }
+
+	
+
