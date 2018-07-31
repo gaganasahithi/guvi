37587e1ec7b9a0c314b88246add712b6abf31943
gaganasahithi <41728944+gaganasahithi@users.noreply.github.com>
2018-07-31 07:11:45 +0100

    Create odd.java

---
+++ prgs/odd.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+
+class odd
+{
+	public static void main (String[] args) throws java.lang.Exception
+{
+	System.out.println("the odd numbers are: ");
+	for(int i=1;i<=6;i++)
+	{
+		if(i % 2 != 0)
+		{
+			System.out.println(i+"");
+		}
+	}
+}
+	}
