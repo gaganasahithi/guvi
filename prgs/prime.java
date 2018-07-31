d2e20196b7005e144117421bd015d348b5a1b8df
gaganasahithi <41728944+gaganasahithi@users.noreply.github.com>
2018-07-31 06:07:32 +0100

    Create prime.java

---
+++ prgs/prime.java
@@ -0,0 +1,28 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+
+class prime
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		int num = 29;
+        boolean flag = false;
+        for(int i = 2; i <= num/2; ++i)
+        {
+            
+            if(num % i == 0)
+            {
+                flag = true;
+                break;
+            }
+        }
+
+        if (!flag)
+            System.out.println("yes");
+        else
+            System.out.println("no");
+    }
+}
+	
