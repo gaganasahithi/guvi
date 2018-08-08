4b5fca7f63f4188d0497366cc3be3ce01cb5daaf
gaganasahithi <41728944+gaganasahithi@users.noreply.github.com>
2018-08-08 07:50:12 +0100

    Create indexarray.java

---
+++ prgs/indexarray.java
@@ -0,0 +1,24 @@
+
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+
+class indexarray
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		int []a=new int[n];
+		for(int i=0;i<n;i++)
+		{
+			a[i]=sc. nextInt();
+		}
+		for(int i=0;i<n;i++)
+		{
+			System.out.println("\n"+a[i]+" "+i);
+		}
+	}
+}
