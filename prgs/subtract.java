177ae259f5182b9f53459c9a48a5b31ad52c5b8c
gaganasahithi <41728944+gaganasahithi@users.noreply.github.com>
2018-08-09 07:13:49 +0100

    Create subtract.java

---
+++ prgs/subtract.java
@@ -0,0 +1,21 @@
+
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+
+class subtract
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	 Scanner sc=new Scanner(System.in);
+		int h1=sc.nextInt();
+		int m1=sc.nextInt();
+		int h2=sc.nextInt();
+		int m2=sc.nextInt();
+		int h3=h1-h2;
+		int m3=m1-m2;
+		System.out.println(h3+" "+m3);
+	}
+}
