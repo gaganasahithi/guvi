ef2318f8f0f3d102202eb16d93b6068d6b2cb29c
gaganasahithi <41728944+gaganasahithi@users.noreply.github.com>
2018-08-09 06:05:41 +0100

    Create primeintervals.java

---
+++ prgs/primeintervals.java
@@ -0,0 +1,35 @@
+
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+
+class primeintervals
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		 int low = 20, high = 50;
+
+        while (low < high)
+        {
+            boolean flag = false;
+
+            for(int i = 2; i <= low/2; ++i) 
+            {
+                
+                if(low % i == 0) 
+                {
+                    flag = true;
+                    break;
+                }
+            }
+
+            if (!flag)
+                System.out.print(low + " ");
+
+            ++low;
+	}
+	
+}
+}
