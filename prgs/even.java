a391b3b6bb58c37153b3e801beef409b07f94e89
gaganasahithi <41728944+gaganasahithi@users.noreply.github.com>
2018-08-09 05:53:39 +0100

    Update even.java

--- prgs/even.java
+++ prgs/even.java
@@ -7,15 +7,7 @@ class even
 {
 	public static void main (String[] args) throws java.lang.Exception
 	{
-	import java.util.*;
-import java.lang.*;
-import java.io.*;
-
-/* Name of the class has to be "Main" only if the class is public. */
-class Ideone
-{
-	public static void main (String[] args) throws java.lang.Exception
-	{
+	
 		Scanner sc=new Scanner(System.in);
 		int n=sc.nextInt();
 		int q=sc.nextInt();
@@ -27,4 +19,4 @@ class Ideone
 		}
 	}
 }
-}
+
