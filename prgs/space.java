398de8751a974350b2f142869681579d65de6cde
gaganasahithi <41728944+gaganasahithi@users.noreply.github.com>
2018-08-10 08:15:08 +0100

    Update space.java

--- prgs/space.java
+++ prgs/space.java
@@ -1,9 +1,8 @@
-
-
 import java.util.*;
 import java.lang.*;
 import java.io.*;
 
+/* Name of the class has to be "Main" only if the class is public. */
 class space
 {
 	public static void main (String[] args) throws java.lang.Exception
