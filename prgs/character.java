f3534fea0e546321d56f822a650bbeaf586b19bb
gaganasahithi <41728944+gaganasahithi@users.noreply.github.com>
2018-08-10 07:36:47 +0100

    Create character.java

---
+++ prgs/character.java
@@ -0,0 +1,27 @@
+
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+
+class character
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	Scanner sc=new Scanner(System.in);
+		String s=sc.nextLine();
+		int count=0,i=0;
+		for(i=0;i<s.length();i++)
+		{
+			if(s.charAt(i)!=' ')
+			{
+				count++;
+			}
+			
+		}
+		System.out.println(""+count);
+	}
+
+}	
+	
