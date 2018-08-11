e7100a902287d2b6bbe4d9c71b7e37c659466e2d
gaganasahithi <41728944+gaganasahithi@users.noreply.github.com>
2018-08-11 08:10:55 +0100

    Create among10.java

---
+++ prgs/among10.java
@@ -0,0 +1,22 @@
+
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+
+class among10
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		 int n=10;
+     int c;
+     Scanner s=new Scanner(System.in);
+     ArrayList<Integer>a=new ArrayList<Integer>();
+     for(int i=0;i<10;i++){
+        c=s.nextInt();
+        a.add(c);
+     }
+   System.out.print(Collections.max(a));
+	}
+}
