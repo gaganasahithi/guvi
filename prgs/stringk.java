1fa14ebb541bc4ad0dfc371ad4d5dee088ac9d0d
gaganasahithi <41728944+gaganasahithi@users.noreply.github.com>
2018-08-13 05:17:08 +0100

    Create stringk.java

---
+++ prgs/stringk.java
@@ -0,0 +1,21 @@
+
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class stringk
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		String a;
+     int k;
+     Scanner s=new Scanner(System.in);
+     k=s.nextInt();
+     a=s.next();
+     for(int i=1;i<=k;i++){
+         System.out.println(a);
+		
+	}
+}
+}
