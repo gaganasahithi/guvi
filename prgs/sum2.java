32c8de1fce00b5c4adf84b7ab268c9a051fa178e
gaganasahithi <41728944+gaganasahithi@users.noreply.github.com>
2018-08-13 08:00:10 +0100

    Create sum2.java

---
+++ prgs/sum2.java
@@ -0,0 +1,17 @@
+
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+
+class sum2
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+        System.out.println(a+b);
+	}
+}
