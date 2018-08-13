c64664abde392ab9ed75402b4711fec7249982ed
gaganasahithi <41728944+gaganasahithi@users.noreply.github.com>
2018-08-13 08:13:00 +0100

    Create power2.java

---
+++ prgs/power2.java
@@ -0,0 +1,28 @@
+
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+
+class power2
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		 int a,b,f=0;
+     Scanner s=new Scanner(System.in);
+     a=s.nextInt();
+     while(a!=1){
+         if(a%2!=0){
+             f=1;
+         }
+         a=a/2;
+        }
+     if(f==1){
+         System.out.println("no");
+     }
+ else{
+         System.out.println("yes");
+ }
+	}
+}
