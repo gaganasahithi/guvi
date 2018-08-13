fb7bc8d4118a689a967f2ace5c722bdcd46ff6a2
gaganasahithi <41728944+gaganasahithi@users.noreply.github.com>
2018-08-13 07:19:42 +0100

    Create numericdigit.java

---
+++ prgs/numericdigit.java
@@ -0,0 +1,26 @@
+
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+
+class numericdigit
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		 Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+
+        int count = 0;
+
+        while(n != 0)
+        {
+            
+            n /= 10;
+            ++count;
+        }
+
+        System.out.println(count);
+	}
+}
