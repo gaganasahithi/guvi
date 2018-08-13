fa0fdbbd923eb1f94991db4ca318d2de499760c3
gaganasahithi <41728944+gaganasahithi@users.noreply.github.com>
2018-08-13 07:32:10 +0100

    Create nextnumber.java

---
+++ prgs/nextnumber.java
@@ -0,0 +1,21 @@
+
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+
+class nextnumber
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		int num;
+	//	int count=0;
+		
+		num=sc.nextInt();
+			num=num+1;
+		
+		System.out.println(num);
+	}
+}
