fe3958d2a8e4d7000a808aefee4e315c9a73fcf2
gaganasahithi <41728944+gaganasahithi@users.noreply.github.com>
2018-08-14 08:22:22 +0100

    Create digitofnum.java

---
+++ prgs/digitofnum.java
@@ -0,0 +1,22 @@
+
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+
+class digitofnum
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner s=new Scanner(System.in);
+		int n=s.nextInt();
+		int count=0;
+		while(n>0)
+		{
+			n = n/ 10;
+			count++;
+		}
+		System.out.print(" " +count);
+	}
+}
