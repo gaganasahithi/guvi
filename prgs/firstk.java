c0d4b67dcfccb3f898c9820d391956b8302d5c73
gaganasahithi <41728944+gaganasahithi@users.noreply.github.com>
2018-08-16 05:54:02 +0100

    Create firstk.java

---
+++ prgs/firstk.java
@@ -0,0 +1,18 @@
+
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+
+class firstk
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner s=new Scanner(System.in);
+		String str=s.next();
+		int n=s.nextInt();
+		for(int i=0;i<n;i++)
+		System.out.print(str.charAt(i));
+	}
+}
