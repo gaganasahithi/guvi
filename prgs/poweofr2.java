6280320c7858fc684ab7694d2f2dcaa7860d4619
gaganasahithi <41728944+gaganasahithi@users.noreply.github.com>
2018-08-17 08:08:47 +0100

    Update and rename power2.java to poweofr2.java

---
+++ prgs/poweofr2.java
@@ -0,0 +1,16 @@
+
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class powerof2
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner s=new Scanner(System.in);
+		int n=s.nextInt();
+		System.out.println(2*n);
+		s.close();
+	}
+}
