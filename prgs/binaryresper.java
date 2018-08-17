41733ca840d3e9cd27220d9f61d1f3d9f1841c44
gaganasahithi <41728944+gaganasahithi@users.noreply.github.com>
2018-08-17 05:54:40 +0100

    Create binaryresper.java

---
+++ prgs/binaryresper.java
@@ -0,0 +1,34 @@
+
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+
+class binaryresper
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		int number=1000;
+		boolean isBinary = true;
+
+		int copyOfNumber = number;
+
+		while (copyOfNumber != 0) {
+			int temp = copyOfNumber % 10; // Gives last digit of the number
+
+			if (temp > 1) {
+				isBinary = false;
+				break;
+			} else {
+				copyOfNumber = copyOfNumber / 10;
+			}
+		}
+
+		if (isBinary) {
+			System.out.println("yes");
+		} else {
+			System.out.println("no");
+		}
+	}
+}
