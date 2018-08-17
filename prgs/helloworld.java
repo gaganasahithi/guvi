e0ffa3eddf19344f2bb243ce66b850f3041524b5
gaganasahithi <41728944+gaganasahithi@users.noreply.github.com>
2018-08-17 07:11:51 +0100

    Create helloworld.java

---
+++ prgs/helloworld.java
@@ -0,0 +1,14 @@
+
+ 
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+ 
+class helloworld
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+        if(System.out.append("hello world").equals(null)){}
+ 
+	}
+}
