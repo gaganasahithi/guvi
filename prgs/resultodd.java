88b282bfc5b0f561ccfea8342a894831fbb3c531
gaganasahithi <41728944+gaganasahithi@users.noreply.github.com>
2018-08-17 07:00:39 +0100

    Create resultodd.java

---
+++ prgs/resultodd.java
@@ -0,0 +1,26 @@
+
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+
+class resultodd
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner s=new Scanner(System.in);
+		int n=s.nextInt();
+		int m=s.nextInt();
+		int p;
+		p=n+m;
+		if(p%2==0)
+		{
+			System.out.print("even");
+		}		
+		else
+		{
+			System.out.print("odd");
+		}
+	}
+	
