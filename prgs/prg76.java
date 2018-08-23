0de3939dcdcae88e2c2bc294d6ba397a1c5ecbe5
gaganasahithi <41728944+gaganasahithi@users.noreply.github.com>
2018-08-23 08:18:46 +0100

    Create prg76.java

---
+++ prgs/prg76.java
@@ -0,0 +1,20 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class prg76
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner in=new Scanner(System.in);
+		String s=in.next();
+		int l=s.length();
+		if(l%2==0)
+			System.out.println(s.substring(0,l/2-1)+"**"+s.substring(l/2+1));
+		else
+			System.out.println(s.substring(0,l/2)+"*"+s.substring(l/2+1));
+	}
+}
