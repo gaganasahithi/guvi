5552d0bc0840cf9da75c93239274226ba733cad5
gaganasahithi <41728944+gaganasahithi@users.noreply.github.com>
2018-08-23 08:26:36 +0100

    Update prg76.java

--- prgs/prg76.java
+++ prgs/prg76.java
@@ -5,16 +5,20 @@ import java.lang.*;
 import java.io.*;
 
 /* Name of the class has to be "Main" only if the class is public. */
-class prg76
+class Ideone
 {
 	public static void main (String[] args) throws java.lang.Exception
 	{
-		Scanner in=new Scanner(System.in);
-		String s=in.next();
-		int l=s.length();
-		if(l%2==0)
-			System.out.println(s.substring(0,l/2-1)+"**"+s.substring(l/2+1));
+	Scanner in=new Scanner(System.in);
+		int n=in.nextInt();
+		int count=0;
+		for(int i=2;i<=n;i++){
+			if(n%i==0)
+				count++;	
+		}
+		if(count==1)
+			System.out.println("no");
 		else
-			System.out.println(s.substring(0,l/2)+"*"+s.substring(l/2+1));
+			System.out.println("yes");
 	}
 }
