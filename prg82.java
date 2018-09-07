e8e51e7de63a4de5a9cace0fa25dfeb1e1118e71
gaganasahithi <41728944+gaganasahithi@users.noreply.github.com>
2018-09-07 09:59:03 +0100

    Create prg82.java

---
+++ prg82.java
@@ -0,0 +1,20 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class prg82
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+    	float n1=sc.nextFloat();
+    	float n2=sc.nextFloat();
+    	float n3;
+    	n3=n1*n2;
+        DecimalFormat df = new DecimalFormat("##0.00000");
+        System.out.println(df.format(n3));
+	}
+}
